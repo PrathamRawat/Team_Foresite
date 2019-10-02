0f1390bebeb9e46f4790055548bc4246e32155fe
jessechen1618 <jchen03@stuy.edu>
2019-10-02 08:41:58 -0400

    Day 1

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,13 +3,22 @@
 # K15 -- ?
 # 2019-10-02
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__);
 
 @app.route("/")
 def whyTho():
-   return render_template('')
+	try:
+		userData = request.cookies
+		username = userData["username"]
+		userpswd = userData["password"]
+	except:
+		return render_template('start.html')
+
+@app.route("/login")
+def logIn():
+	return 0
 
 if __name__ == "__main__":
     app.debug = True
