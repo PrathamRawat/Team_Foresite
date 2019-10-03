3c64e086b8bd8b12b1e13db19e669f6bcb742f1c
PrathamRawat <prathamrawat815@gmail.com>
2019-10-03 08:05:36 -0400

    Implement basics for login

--- 15_login/app.py
+++ 15_login/app.py
@@ -9,16 +9,30 @@ app = Flask(__name__);
 
 @app.route("/")
 def whyTho():
-	try:
-		userData = request.cookies
-		username = userData["username"]
-		userpswd = userData["password"]
-	except:
-		return render_template('start.html')
+    try:
+        userData = request.cookies
+        username = userData["username"]
+        userpswd = userData["password"]
+        return checkCredentials(username, userpswd)
+    except:
+        return render_template('start.html')
 
-@app.route("/login")
+@app.route("/login", methods=['POST'])
 def logIn():
-	return 0
+    return render_template('start.html')
+
+def checkCredentials(username, password):
+    userData = open("data/accounts.csv");
+    accountsDict = {}
+    for line in userData:
+        try:
+            if(accountsDict[username] != password):
+                print("Incorrect Password")
+                return render_template("start.html", error = "Incorrect Password!")
+        except: #Will run if the username does not exist in the Dictionary
+            print("No username found!")
+            return render_template("start.html", error = "Incorrect Username!")
+
 
 if __name__ == "__main__":
     app.debug = True
